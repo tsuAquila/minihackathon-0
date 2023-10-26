9b46cd66c699d023d1c5b7302b72827cdbaf61b2
Siby C R <sibycr18@gmail.com>
2023-10-26 15:35:58 +0530

    for sake of commit

---
+++ sibycr18/README.md
@@ -0,0 +1,18 @@
+# Siby C.R.
+
+### Team Members:
+- Me
+- Myself &
+- I
+
+### Project Title:
+[Your Project's Title]
+
+### Description:
+[Brief description of what you built]
+
+### How we Built It:
+[Brief description of the tools, languages, or technologies used]
+
+### How to Use It:
+[Brief instructions on how to use your project]
---
+++ sibycr18/game.py
@@ -0,0 +1,112 @@
+from random import randint
+from random import choice
+import inflect
+p = inflect.engine()
+
+# Read and display scores from the text file
+with open("cricket_scores.txt", "r") as file:
+    high_scores = file.readlines()
+
+print("\n**High Scores**")
+for score in high_scores:
+    print(score.strip())  # Print each score, stripping newline characters
+
+# If you want to display the top N scores, you can modify the loop like this:
+# for index, score in enumerate(scores[:N], start=1):
+#     print(f"{index}. {score.strip()}")
+
+# Format of dict -> {question number : [question(with options), correct_choice]}
+trivia_questions = {
+    1 : ["Which of these cricket tournaments is played between international teams? \n(a) IPL\n(b) PSL\n(c) T20 World Cup\n(d) Big Bash League", "c"],
+    2 : ["How many players are in a cricket team? \n(a) 9\n(b) 10\n(c) 11\n(d) 12", "c"],
+    3 : ["Where is the sports stadium, ‘Green Park’ located? \n(a) Kanpur\n(b) Jamshedpur\n(c) Cutlack\n(d) Patiala", "a"],
+    4 : ["The international governing body of cricket is? \n(a) CCI\n(b) GCI\n(c) LIC\n(d) ICC", "d"],
+    5 : ["Which of these teams has won the most ICC Champions Trophy titles? \n(a) India\n(b) Australia\n(c) South Africa\n(d) Sri Lanka", "b"]
+}
+trivia_score = 0
+
+your_name = input("\nEnter your name: ")
+
+print("\n\t--- Answer 5 cricket trivia questions. Your trivia score will decide the player you get to bat... Good luck! ---\n")
+print("\t\t\t*** Note that players with more ratings score more runs ***\n")
+
+
+available_questions = list(trivia_questions.keys())
+for i in range(5):
+    question_number = choice(available_questions)
+    print('Q) ' + trivia_questions[question_number][0])
+    annswer = input("\nEnter your choice: ").strip().strip(')').strip('(').lower()
+    if annswer == trivia_questions[question_number][1]:
+        print("\nCorrect Answer...")
+        trivia_score += 1
+        print("Current Score: ", trivia_score)
+    else:
+        print("\nIncorrect Answer...")
+    print()
+    available_questions.remove(question_number)
+print("\nYou Scored", trivia_score)
+
+players = {
+    9 : ["Virat Kohli", "Rohit Sharma", "Ravindra Jadeja"],
+    7 : ["Shreyas Iyer", "KL Rahul", "Shubhman Gill"],
+    5 : ["Shikhar Dhawan", "Ishan Kishan", "Washington Sundar"]
+}
+if trivia_score <= 2:
+    your_player_rating = 5
+elif trivia_score <= 4:
+    your_player_rating = 7
+else:
+    your_player_rating = 9
+
+your_player = choice(players[your_player_rating])
+print("\nYour player is " + your_player)
+print("Rating:", your_player_rating)
+
+print("\n\n\tLETS BEGIN THE MATCH!")
+print("You'll get 1 over for scoring maximum runs... Good luck!\n\n")
+
+runs_scored = 0
+# def runout():
+#     pass
+
+
+# Define bowlers with different types: fast, medium, and slow
+bowlers = {
+    "fast": {"chance_of_out": 0.7, "runs_conceded": [0, 1, 2, 4, 6]},
+    "medium": {"chance_of_out": 0.5, "runs_conceded": [0, 1, 2, 3, 4]},
+    "slow": {"chance_of_out": 0.3, "runs_conceded": [0, 1, 1, 2, 3]}
+}
+
+# Determine the type of bowler based on random choice
+bowler_type = choice(["fast", "medium", "slow"])
+
+print(f"You got a {bowler_type.capitalize()} bowler...")
+
+# Simulate one over of the match
+for i in range(6):  # 1 over contains 6 balls
+    # Simulate the bowler's delivery
+    is_out = randint(1, 10) <= bowlers[bowler_type]["chance_of_out"]
+    
+    if not is_out:  # Player is not out, continue playing
+        runs_this_ball = choice(bowlers[bowler_type]["runs_conceded"])
+        print(f"\nYou scored {runs_this_ball} runs on {p.ordinal(i+1)} ball!")
+        runs_scored += runs_this_ball
+    else:  # Player is out
+        print("\nOh no! You're out!")
+        break
+
+print("\nIn your 1 over, you scored", runs_scored, "runs.")
+
+# Higher-rated players have a better chance of not getting out and scoring more runs
+if your_player_rating == 9:
+    runs_scored *= 2  # Double the runs scored by the highest-rated player
+elif your_player_rating == 7:
+    runs_scored += 2  # Add 2 runs for the medium-rated player
+
+print("\nTotal runs scored:", runs_scored)
+
+# Store scores in a text file
+with open("cricket_scores.txt", "a") as file:
+    file.write(f"{your_name} {runs_scored}\n")
+
+# print("\nScores have been saved in 'cricket_scores.txt' file.")
