561eb550ea7f56263066925a31c0317d3cd710c6
Siby C R <sibycr18@gmail.com>
2023-10-26 19:43:49 +0530

    enhanced play

--- sibycr18/cricket.py
+++ sibycr18/cricket.py
@@ -8,7 +8,7 @@ from time import sleep
 with open("cricket_scores.txt", "r") as file:
     high_scores = file.readlines()
 
-print("\n**High Scores**")
+print("\n**Scoreboard**")
 for score in high_scores:
     print(score.strip())  # Print each score, stripping newline characters
 
@@ -29,7 +29,7 @@ trivia_score = 0
 your_name = input("\nEnter your name: ")
 
 print("\n\t--- Answer 5 cricket trivia questions. Your trivia score will decide the player you get to bat... Good luck! ---\n")
-print("\t\t\t*** Note that players with more ratings score more runs ***\n")
+print("\t\t\t*** Note that players with more ratings score more runs and have more wickets ***\n")
 
 
 available_questions = list(trivia_questions.keys())
@@ -54,31 +54,31 @@ players = {
 }
 if trivia_score <= 2:
     your_player_rating = 5
+    wickets_left = 0
 elif trivia_score <= 4:
     your_player_rating = 7
+    wickets_left = 1
 else:
     your_player_rating = 9
+    wickets_left = 2
 
 your_player = choice(players[your_player_rating])
 print("\nYour player is " + your_player)
 print("Rating:", your_player_rating)
 
-print("\n\tLETS BEGIN THE MATCH!")
+print("\n\tLETS BEGIN THE MATCH!\n")
 
 runs_scored = 0
-# def runout():
-#     pass
-
 
 # Define bowlers with different types: fast, medium, and slow
 bowlers = {
-    "fast": {"chance_of_out": 0.7, "runs_conceded": [0, 1, 2, 4, 6]},
-    "medium": {"chance_of_out": 0.5, "runs_conceded": [0, 1, 2, 3, 4]},
-    "slow": {"chance_of_out": 0.3, "runs_conceded": [0, 1, 1, 2, 3]}
+    "fast": {"chance_of_out": 3, "runs_conceded": [0, 1, 2, 4, 6]},
+    "spin": {"chance_of_out": 2, "runs_conceded": [0, 1, 2, 3, 4]},
+    "slow": {"chance_of_out": 1, "runs_conceded": [0, 1, 1, 2, 3]}
 }
 
 # Determine the type of bowler based on random choice
-bowler_type = choice(["fast", "medium", "slow"])
+bowler_type = choice(["fast", "spin", "slow"])
 
 print(f"You got a {bowler_type.capitalize()} bowler...")
 
@@ -86,25 +86,34 @@ print(f"You got a {bowler_type.capitalize()} bowler...")
 for i in range(6):  # 1 over contains 6 balls
     # Simulate the bowler's delivery
     is_out = randint(1, 10) <= bowlers[bowler_type]["chance_of_out"]
+    # print(f'{is_out = }')
     
     if not is_out:  # Player is not out, continue playing
         runs_this_ball = choice(bowlers[bowler_type]["runs_conceded"])
         print(f"You scored {runs_this_ball} runs on {p.ordinal(i+1)} ball!")
         runs_scored += runs_this_ball
     else:  # Player is out
-        print("\nOh no! You're out!")
-        break
-    sleep(1)
+        print(f"\nOh no! You're out on {p.ordinal(i+1)} ball!\n")
+        # print("--Answer this question to get more life--")
+        if wickets_left != 0:
+            print(f"You got {wickets_left} more wickets left...")
+            continue
+        else:
+            print(f"No more wickets left...")
+            break
+
+    sleep(0.5)
 
 print("\nIn your 1 over, you scored", runs_scored, "runs.")
 
 # Higher-rated players have a better chance of not getting out and scoring more runs
 if your_player_rating == 9:
     runs_scored *= 2  # Double the runs scored by the highest-rated player
+    print("\nTotal runs scored(x2 for 9 rated player):", runs_scored)
 elif your_player_rating == 7:
     runs_scored += 2  # Add 2 runs for the medium-rated player
+    print("\nTotal runs scored (+2 for 7 rated player):", runs_scored)
 
-print("\nTotal runs scored:", runs_scored)
 
 # Store scores in a text file
 with open("cricket_scores.txt", "a") as file:
